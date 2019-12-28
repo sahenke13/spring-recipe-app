d51e6a1f63578753cc2555111a82128fd3b77982
Steven Henke <steven@foundrymakes.com>
2019-12-28 08:10:27 -0600

    init commit

---
+++ src/main/java/founrdy/steven/RecipeAppApplication.java
@@ -0,0 +1,15 @@
+package founrdy.steven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeAppApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(RecipeAppApplication.class, args);
+    }
+
+}
